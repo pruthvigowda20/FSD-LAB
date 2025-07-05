363890c83577cd50b095d6ed4dd7308e573ec5f7
pruthvigowda20 <Pruthvivvp69@gmail.com>
2025-07-05 09:17:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function submit(){
+    debugger
+    fname=document.getElementById('fname')
+    lname=document.getElementById('lname')
+    fullname=fname.value+" "+lname.value
+    console.log(fullname)
+    phno=document.getElementById('phno')
+    console.log(phno.value)
+    Address=document.getElementById('Address')
+    console.log(Address.value)
+    password=document.getElementById('password')
+    console.log(password.value)
+
+}
